56b2c5f4f170d26a33e148804f6d06e3a1c08116
SimoneCerri <simone.cerri1994@gmail.com>
2024-05-31 16:31:40 +0200

    add a Jumbo as a component to re-use with different props

---
+++ src/components/AppJumbostronz.vue
@@ -0,0 +1,27 @@
+<script>
+export default
+    {
+        name: 'AppJumbostronz',
+        props:
+        {
+            'pageTitle': String,
+            'link': String,
+            'btnTitle': String,
+        }
+    }
+</script>
+
+<template>
+    <section class="bg-dark text-danger">
+        <div class="container my_h_small d-flex align-items-center justify-content-between">
+            <h1>
+                <strong>
+                    {{ pageTitle }}
+                </strong>
+            </h1>
+            <RouterLink class="btn btn-secondary text-danger fw-bold" :to="{ name: link }">{{ btnTitle }}</RouterLink>
+        </div>
+    </section>
+</template>
+
+<style></style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,12 +1,18 @@
 <script>
+import AppJumbostronz from "../components/AppJumbostronz.vue";
+
 export default
     {
         name: 'AppHome',
+        components:
+        {
+            AppJumbostronz,
+        }
     }
 </script>
 
 <template>
-    Home page
+    <AppJumbostronz page-title="HOME" link="projects" btn-title="Projects" />
 </template>
 
 <style></style>
